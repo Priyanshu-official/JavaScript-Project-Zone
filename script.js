64a14b3566a762d34fd8931a32da0e2606e9a39c
Priyanshu <Priyanshukaushal878@gmail.com>
2021-06-11 22:40:57 +0530

    Add: Created API for Project list and Intgrated

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
 var project = document.querySelector('.projectItem')
 
-var projectItems = [
-    {
-        Name:"Mind Game",
-        Author:"Priyanshu",
-        About: "Basic Java Script Project. Learned about function and Manupulate data usinf JS",
-        link:"#",
-        Source:"https://github.com/Priyanshu-official/JavaScript-Project/tree/main/Mind_Game",
-        Demo:"./Demo/Mind_Game.gif"
-    },
-    {
-        Name:"Courses in Order",
-        Author:"Priyanshu",
-        About: "Basic Java Script Project. implemented Loginc and arrange in order",
-        link:"#",
-        Source:"https://github.com/Priyanshu-official/JavaScript-Project/tree/main/Genrating_Cart_List",
-        Demo:"./Demo/Course_Cart.gif"
-    },
-    {
-        Name:"Loding Number",
-        Author:"Priyanshu",
-        About: "Basic Java Script Project. Followers lodind Script",
-        link:"#",
-        Source:"https://github.com/Priyanshu-official/JavaScript-Project/tree/main/Number_loding",
-        Demo:"./Demo/Loding_Number.gif"
-    },
-]
-
 
 function ProjectContainer() {
     console.log("Running");
     var output = ""
 
-    projectItems.forEach(({Name,Author,link,Source,About,Demo}) => {
-        output += `<div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="${Demo}" alt="content">
-          <h2 class="text-2xl text-gray-900 font-medium title-font">${Name}</h2>
-          <h3 class="text-geay-400 text-base font-medium title-font">Developer:<span class="text-black ml-2">${Author}</span> </h3>
-          <hr class="h-0.5 rounded bg-gray-600"></hr>
-          <p class="leading-relaxed text-base mt-2 mb-2">${About}</p>
-          <a herf=${link} class="inline-flex items-center bg-black border-0 py-2 px-3 focus:outline-none hover:bg-gray-900 rounded text-base text-white mt-6 md:mt-0">View Project
-          </a>
-          <a href=${Source} class="inline-flex items-center bg-yellow-300 border-0 py-2 px-3 focus:outline-none hover:bg-yellow-200 rounded text-base text-black mt-6 md:mt-0">Source Code
-        </a>
-        </div>
-      </div>
-        `
-    })
-    project.innerHTML = output
+    /* Fetch DATA */
+    var fetchData = async ()=>{
+        var reponse = await fetch('http://localhost:8000/project_Item');
+        var todo = await reponse.json();
+
+        todo.forEach(({projectName,projectAuthor,projectAbout,projectDemo,projectLink,projectSource}) => {
+            output += `<div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="bg-gray-100 p-6 rounded-lg">
+              <img class="h-40 rounded w-full object-cover object-center mb-6" src="${projectDemo}" alt="content">
+              <h2 class="text-2xl text-gray-900 font-medium title-font">${projectName}</h2>
+              <h3 class="text-geay-400 text-base font-medium title-font">Developer:<span class="text-black ml-2">${projectAuthor}</span> </h3>
+              <hr class="h-0.5 rounded bg-gray-600"></hr>
+              <p class="leading-relaxed text-base mt-2 mb-2">${projectAbout}</p>
+              <a herf="${projectLink}" class="inline-flex items-center bg-black border-0 py-2 px-3 focus:outline-none hover:bg-gray-900 rounded text-base text-white mt-6 md:mt-0">View Project
+              </a>
+              <a href=${projectSource} class="inline-flex items-center bg-yellow-300 border-0 py-2 px-3 focus:outline-none hover:bg-yellow-200 rounded text-base text-black mt-6 md:mt-0">Source Code
+            </a>
+            </div>
+          </div>
+            `
+        })
+        project.innerHTML = output
+    }
+
+    /* Calling API */
+    window.onload = () =>{
+        fetchData();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", ProjectContainer);
